9f30698c5d46fa32988a23a356be038161433aa5
Jame Chaney <macneilslt@gmail.com>
2023-04-17 09:31:57 -0400

    The app component with nc header

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 
 function App() {
 	return <div className="App">
-		hello Nucamp!
+		<Navbar dark colr='primary' sticky='top' expand='md'>
+			<Container>
+				<NavbarBrand href='/'>
+					<img src={NucampLogo} alt='nucamp logo' />
+				</NavbarBrand>
+			</Container>
+		</Navbar>
+		I'm ready for Workshop!
 	</div>;
 }
 
