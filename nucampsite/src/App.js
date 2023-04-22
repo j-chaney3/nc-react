266e74aeb745f1c2b1ae61e8b9a8716fe1a0ba17
Jame Chaney <macneilslt@gmail.com>
2023-04-22 13:58:15 -0400

    Updated App with new components/features

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 import CampsitesList from './features/campsites/CampsitesList';
-import {CAMPSITES} from './app/shared/CAMPSITES';
+import { CAMPSITES } from './app/shared/CAMPSITES';
 
 function App() {
-	return <div className="App">
-		<Navbar dark colr='primary' sticky='top' expand='md'>
-			<Container>
-				<NavbarBrand href='/'>
-					<img src={NucampLogo} alt='nucamp logo' />
-				</NavbarBrand>
-			</Container>
-		</Navbar>
-		<CampsitesList />
-	</div>;
+	return (
+		<div className="App">
+			<Header />
+			<CampsitesList />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
