20dbb052ee1e0162b98e5792cf94a992f866b226
Jame Chaney <macneilslt@gmail.com>
2023-04-25 23:57:03 -0400

    Cleaned imports

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,10 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 import CampsitesList from './features/campsites/CampsitesList';
-import { CAMPSITES } from './app/shared/CAMPSITES';
 
 function App() {
 	return (
