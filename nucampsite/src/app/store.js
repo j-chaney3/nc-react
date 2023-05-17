b0f3480084ea86c41e3b601cb5a66d60b32aef93
Jame Chaney <macneilslt@gmail.com>
2023-05-17 11:52:04 -0400

    Async Camptsites

--- nucampsite/src/app/store.js
+++ nucampsite/src/app/store.js
@@ -4,7 +4,7 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
-import { userReducer } from '../features/users/userSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
