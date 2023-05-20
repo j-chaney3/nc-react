15186489c66d568fa8341320aa580e813e23e622
Jame Chaney <macneilslt@gmail.com>
2023-05-20 12:31:58 -0400

    comments json

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -10,8 +10,9 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
 import {fetchCampsites} from './features/campsites/campsitesSlice';
 import {fetchPartners } from './features/partners/partnersSlice';
-import './App.css';
 import { fetchPromotions } from './features/promotions/promotionsSlice';
+import { fetchComments } from './features/comments/commentsSlice';
+import './App.css';
 
 function App() {
 	const dispatch = useDispatch();
@@ -20,6 +21,7 @@ function App() {
 		dispatch(fetchCampsites());
 		dispatch(fetchPartners());
 		dispatch(fetchPromotions());
+		dispatch(fetchComments());
 	}, [dispatch]);
 
 	return (
